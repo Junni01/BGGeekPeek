ffb83816459e3764336ed062b4a86c58b1f4d1ca
Junni01 <junnisaku@gmail.com>
2017-12-10 20:18:23 +0200

    Hottest working

--- GeekJava.js
+++ GeekJava.js
@@ -3,9 +3,11 @@ var hotName = "hottest game";
 
 var xmlhttp = new XMLHttpRequest();
 var bggAPIurl = "https://www.boardgamegeek.com/xmlapi2/";   // This is the root url of the API were using, we can append commands to this.
-var jsonObj
-
-
+var jsonObj;
+var raakaData;
+var games;
+var hotName;
+var games2;
 
 function displayHottest() {
 
@@ -22,16 +24,27 @@ function doOnLoad()
 
 function getHotness() {
 
-    var url = "https://bgg-json.azurewebsites.net/hot";
+    var url = bggAPIurl + "hot?type=boardgame";
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            raakaData = xmlhttp.responseText;
-            jsonObj = JSON.parse(xmlhttp.responseText);
+            raakaData = xmlhttp.responseXML;
+            games = raakaData.getElementsByTagName("name");
+            hotName = games[0].getAttribute("value");
+            games2 = raakaData.getElementsByTagName("thumbnail");
+            var hotImg = games2[0].getAttribute("value");
+
+
+            $("#hottestName").html(hotName);
+            $("#hottestImg").html("<img src=" + hotImg +">");
+
         }
 
 
     }
+
+
+
 }
